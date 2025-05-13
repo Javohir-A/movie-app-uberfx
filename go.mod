d67927433d94f60eb17c6f81f07f693871ed3611
javohir-a <abdusamatovjavohir@gmail.com>
2025-05-13 17:48:20 +0500

    get single and update field methods implemented

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.34.0
 	github.com/spf13/cast v1.8.0
+	go.uber.org/fx v1.23.0
 	go.uber.org/zap v1.27.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.26.1
@@ -21,6 +22,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	go.uber.org/dig v1.18.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.9.0 // indirect
--- internal/usecase/repo/movie.go
+++ internal/usecase/repo/movie.go
@@ -66,3 +66,58 @@ func (r *MovieRepo) Create(ctx context.Context, req model.Movie) (model.Movie, e
 
 	return req, nil
 }
+func (r *MovieRepo) GetSingle(ctx context.Context, req model.Id) (model.Movie, error) {
+	var movie model.Movie
+
+	if err := r.db.WithContext(ctx).
+		First(&movie, req.ID).Error; err != nil {
+		return model.Movie{}, err
+	}
+
+	var cast []model.Actor
+	if err := r.db.WithContext(ctx).
+		Table("actors").
+		Select("actors.*").
+		Joins("JOIN movie_actors ON movie_actors.actor_id = actors.id").
+		Where("movie_actors.movie_id = ?", movie.ID).
+		Find(&cast).Error; err != nil {
+		return model.Movie{}, err
+	}
+
+	movie.Cast = cast
+	return movie, nil
+}
+
+func (r *MovieRepo) UpdateField(ctx context.Context, req model.UpdateFieldRequest) (model.RowsEffected, error) {
+	db := r.db.WithContext(ctx).Model(&model.Movie{})
+
+	// Apply filters
+	for _, f := range req.Filter {
+		switch f.Type {
+		case "eq":
+			db = db.Where(f.Column+" = ?", f.Value)
+		case "ne":
+			db = db.Where(f.Column+" <> ?", f.Value)
+		case "gt":
+			db = db.Where(f.Column+" > ?", f.Value)
+		case "lt":
+			db = db.Where(f.Column+" < ?", f.Value)
+		case "gte":
+			db = db.Where(f.Column+" >= ?", f.Value)
+		case "lte":
+			db = db.Where(f.Column+" <= ?", f.Value)
+		case "search":
+			db = db.Where(f.Column+" ILIKE ?", "%"+f.Value+"%")
+		}
+	}
+
+	// Build update map
+	updateMap := map[string]interface{}{}
+	for _, item := range req.Items {
+		updateMap[item.Column] = item.Value
+	}
+
+	// Execute update
+	tx := db.Updates(updateMap)
+	return model.RowsEffected{RowsEffected: int(tx.RowsAffected)}, tx.Error
+}
